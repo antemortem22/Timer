7776560f6361207ef6fe6afda44cee8093407144
antemortem22 <agosdn123@gmail.com>
2022-08-18 19:56:27 -0300

    first update

---
+++ timer.js
@@ -0,0 +1,6 @@
+let inputs, clock, alarm, hours, minutes, seconds, repeater; // Declare variables
+
+window.addEventListener('load', () => { 
+    inputs = Array.from(document.getElementsByClassName('number'));
+    clock = document.querySelector('.clock');
+});
