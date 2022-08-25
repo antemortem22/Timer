17d4e1474c8179f51dff1787a8dc9a6fe2e37691
antemortem22 <agosdn123@gmail.com>
2022-08-25 17:52:10 -0300

    second update

--- timer.js
+++ timer.js
@@ -3,4 +3,22 @@ let inputs, clock, alarm, hours, minutes, seconds, repeater; // Declare variable
 window.addEventListener('load', () => { 
     inputs = Array.from(document.getElementsByClassName('number'));
     clock = document.querySelector('.clock');
+    alarm = new Audio('sound/alarm.mp3');
 });
+
+function startTimer(){
+    parseTime();
+}
+
+function parseTime(){
+    hours = Number(inputs[0].value);
+    minutes = Number(inputs[1].value);
+    seconds = Number(inputs[2].value);
+}
+
+function setTimer(){
+    clock.innerHTML = `<p class="number">${hours > 9 ? hours : ('0' + hours)}</p><span>hs</span><p
+    class="number"> ${minutes > 9 ? minutes : ('0' + minutes)}</p><span>min</span><p
+    class="number">${seconds > 9 ? seconds : ('0' + seconds)}</p><span>sec</span>`
+    
+}
